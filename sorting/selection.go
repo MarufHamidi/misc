8b6df24c3af6efc2928d3053164873b631e51c03
Maruf Hamidi <hamidimaruf@gmail.com>
2018-02-18 22:27:53 +0600

    selection sort added

---
+++ sorting/selection.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+var inputFile = "input"
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func read() []int {
+	dat, err := ioutil.ReadFile(inputFile)
+	check(err)
+	datStr := strings.TrimSpace(string(dat))
+	datStrArr := strings.Split(datStr, " ")
+	var arr []int
+	for i := 0; i < len(datStrArr); i++ {
+		n, _ := strconv.Atoi(datStrArr[i])
+		arr = append(arr, n)
+	}
+	return arr
+}
+
+// selection sort
+func sort(data []int) []int {
+	arr := data
+	for i := 0; i < len(arr)-1; i++ {
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] > arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+	return arr
+}
+
+func main() {
+	sorted := sort(read())
+	fmt.Println(sorted)
+}
